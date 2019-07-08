129a1d954b9294b058b3f911ef7f89c42758fd43
flowing_clouds_xy <wiserxin@163.com>
2019-07-08 20:32:34 +0800

    加入 __name__=='__main__' 的判断

--- mainFrame.py
+++ mainFrame.py
@@ -2,25 +2,25 @@
 import tkinter as tk
 import wordRoot as wr
 
-
-root=tk.Tk()
-root.title('="w"=')
-root.geometry('600x310')
-
-
-
-e=tk.Entry(root,font=("黑体", 20, "bold"))
-t=tk.Text(root,height=10,font=("黑体", 20, "bold"))
+if __name__=='__main__':
+    root=tk.Tk()
+    root.title('="w"=')
+    root.geometry('600x310')
 
 
-e.pack()#side = tk.TOP
-t.pack(side = tk.BOTTOM)
 
-def enterAChar(event):
-    print(e.get())
-    t.delete(1.0,tk.END)
-    t.insert('end',wr.getM(e.get()))
+    e=tk.Entry(root,font=("黑体", 20, "bold"))
+    t=tk.Text(root,height=10,font=("黑体", 20, "bold"))
 
-e.bind("<KeyRelease>", enterAChar)
 
-root.mainloop()
+    e.pack()#side = tk.TOP
+    t.pack(side = tk.BOTTOM)
+
+    def enterAChar(event):
+        print(e.get())
+        t.delete(1.0,tk.END)
+        t.insert('end',wr.getM(e.get()))
+
+    e.bind("<KeyRelease>", enterAChar)
+
+    root.mainloop()
